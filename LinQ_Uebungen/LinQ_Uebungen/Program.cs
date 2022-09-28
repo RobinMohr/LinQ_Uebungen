14fa51f5241cce5c17706fb1f8864e793c9369ac
Mohr, Robin <robin.mohr@tecalliance.net>
2022-09-28 10:26:57 +0200

    Aufgabe 1+2 mit where abfrage

--- LinQ_Uebungen/LinQ_Uebungen/Program.cs
+++ LinQ_Uebungen/LinQ_Uebungen/Program.cs
@@ -9,72 +9,59 @@ namespace LinQ_Uebungen
     internal class Program
     {
         static void Main(string[] args)
-        {/*
+        {
             //Aufgabe 1
 
-            List<int> even = new List<int>();
-            List<int> odd = new List<int>();
 
-            Console.WriteLine("Wie viele Nummern sollen in der zufällig generierten Liste sein?");
+            Console.WriteLine("Wie viele Nummern sollen in der Liste sein?");
             int amount = Convert.ToInt32(Console.ReadLine());
 
-            List<int> rdm_numbers = List_With_Numbers(amount);
-            
+            List<int> numbers = Rdm_Numbers(amount);
 
-            foreach (int num in rdm_numbers)
-            {
-                if (num % 2 == 0)
-                {
-                    even.Add(num);
-                }
-                else
-                {
-                    odd.Add(num);
-                }
-            }
+            var even = from nums in numbers
+                       where nums % 2 == 0
+                       select nums;
 
-            Console.WriteLine("Gerade Zahlen:");
+            Console.WriteLine("Das sind die Geraden Zahlen");
             foreach (int num in even)
             {
                 Console.WriteLine(num);
             }
-            Console.WriteLine();
 
             //Aufgabe 2
 
-            Console.WriteLine("Ungerade Zahlen:");
+            var odd = from nums in numbers
+                       where nums % 2 == 0
+                       select nums;
+
+            Console.WriteLine("Das sind die Ungeraden Zahlen");
             foreach (int num in odd)
             {
                 Console.WriteLine(num);
             }
 
-            //Aufgabe 3*/
 
-            var varr1 = new[] { 3, 9, 2, 8, 6, 5 };
-            foreach (int num in varr1)
-            {
-                if (num * num > 20)
-                {
-                    Console.WriteLine(num);
-                    Console.WriteLine(num*num);
-                }
-            }
+
 
 
 
 
             Console.ReadKey();
+
+
         }
-        static List<int> List_With_Numbers(int amount)
+        static List<int> Rdm_Numbers(int amount)
         {
-            List<int> list = new List<int>();
             Random random = new Random();
+            List<int> ints = new List<int>();
 
             for (int i = 0; i < amount; i++)
-            {                
-                list.Add(random.Next(0, 1001));
+            {
+                ints.Add(random.Next(1,1001));
             }
-            return list;
+            
+            return ints;
         }
+
     }
 }
